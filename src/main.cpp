416e56e5bd32452f13b13a5295044cec19c7c706
wenyefengjames <wenye_feng@163.com>
2026-07-18 21:37:23 +0100

    Simple implementation of main.cpp to test the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <optimizer.h>
 
 int main() {
+    
+    std::string mode_input;
+    std::string mom_input;
+    bool race_mode = false;
+    bool mom = false;
+
     std::cout << "ERS Optimizer Program" << std::endl;
+    std::cout << "Enter 'race' or 'qualify' to choose mode: ";
+    std::cin >> mode_input;
+    std::cout << "Within 1 second of the car ahead?: ";
+    std::cin >> mom_input;
+
+    if(mode_input == "race"){
+        race_mode = true;
+    }
+    if(mom_input == "yes"){
+        mom = true;
+    }
+
+    Optimizer ems = Optimizer(race_mode, mom);
+    if(race_mode){ // long distance
+        ems.main_optimizing_loop(0.0, 4.0, 4.0);
+    }
+    else{ // qualifying
+        ems.main_optimizing_loop(0.0, 4.0, 0);
+    }
+
     return 0;
 }
