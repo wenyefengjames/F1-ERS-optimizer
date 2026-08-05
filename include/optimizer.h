cb46cbca8fca63167e7e05910af8c371db3bcf72
wenyefengjames <wenye_feng@163.com>
2026-08-05 17:03:13 +0100

    Reversing changes made for testing

--- include/optimizer.h
+++ include/optimizer.h
@@ -20,12 +20,13 @@ class Optimizer{
         const int battery_buckets = 4.0/bucket_size + 1;
         bool race_mode;
         bool mom;
-        // Track circuit = Track();
+        // Track circuit = Track(); TO DO REMOVE THIS COMMENT AFTER TESTING
         Car car = Car(race_mode, mom);
         const int harvest_buckets = car.get_battery().get_harvest_limit() / bucket_size + 1;
         std::vector<double> table;    // Table for memoization
         std::vector<std::optional<Option>> choice;   // Table for path reconstruction
 
+        // TO DO REMOVE THIS COMMENT AFTER TESTING
         // std::vector<Option> option_table_straight(int seg_index, double initial_battery);
         // std::vector<Option> option_table_slowcorner(int seg_index);
         // std::vector<Option> option_table_fastcorner(int seg_index, double initial_battery);  
