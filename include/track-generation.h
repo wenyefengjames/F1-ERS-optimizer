849837f6e1e7e6e6c44d54823ef2d8232ad3f9e1
wenyefengjames <wenye_feng@163.com>
2026-08-20 21:13:44 +0100

    Tuned physics coeffcient to fit more accurately to the new track model. Updated others to fit the new change

--- include/track-generation.h
+++ include/track-generation.h
@@ -10,10 +10,8 @@ struct TrackDataPoint{
 
 namespace track_gen{
     inline const std::string TRACK_CSV_FOLDER = "data/track-data/";
-    inline constexpr int MOVING_AVERAGE_WINDOW_SIZE = 7;
 
-    // TO DO
-    // Generate a QSS simulation for the given track
+    // Generate a QSS simulation for the track (TUMFTM Silverstone.csv data source)
     // Inputs: file_name, the file location within /data/track-data/
     // Outputs: a vector of speed as a result of the simulation
     const std::vector<double> qss(const std::string& file_name);
@@ -21,4 +19,9 @@ namespace track_gen{
     // Writes the calculated speed, curvature etc into a file
     // Inputs: file_name, the file location to run QSS simulation on
     void write_csv(const std::string& file_name);
+
+    // Computes curvature via a periodic cubic spline's exact derivatives, instead
+    // of the finite-difference approach in compute_curvature(). Not currently used
+    // by qss() -- kept available in case a noisier data source needs it later.
+    const std::vector<double> compute_curvature_spline(const std::vector<TrackDataPoint>& track_data);
 }
--- test/track-generation-test.cpp
+++ test/track-generation-test.cpp
@@ -7,22 +7,7 @@ namespace t = track_gen;
 int main(){
 
     auto qss = t::qss("");
-    // auto read_csv = t::read_csv("silverstone_antonelli_quali.csv");
-    // auto compute_curvature = t::compute_curvature(read_csv);
-    // auto compute_vmax = t::compute_vmax(compute_curvature);
-
     t::write_csv("");
-
-    // for(size_t i = 0; i < qss.size(); i++){
-    //     std::cout << "QSS speed: " << qss[i] * 3.6 << '\t';
-    //     std::cout << "CSV distance: " << read_csv[i].distance_m << '\t';
-    //     std::cout <<  "x position: " << read_csv[i].x_pos << '\t';
-    //     std::cout <<  "y position: " << read_csv[i].y_pos << '\t';
-    //     std::cout << "compute_curvature: " << compute_curvature[i] << '\t';
-    //     std::cout << "compute_vmax: " << compute_vmax[i] * 3.6 << '\n';
-    // }
 
     // g++ -std=c++20 .\test\track-generation-test.cpp .\src\track-model\track-generation.cpp .\src\physics.cpp -o test_track_gen.exe
 }
-
-
